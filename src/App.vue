9a2f915e77d4c02495338a8c418df6cad2bf2f3e
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-22 13:19:52 +0200

    feat: added PageHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <script>
-export default {}
+import PageHeader from './components/PageHeader.vue';
+
+export default {
+  components: {
+    PageHeader
+  },
+  data() {
+    return {
+      links: [
+      {
+        name: 'Home',
+      },
+      {
+        name: 'Who We Are'
+      },
+      {
+        name: 'What We Do'
+      },
+      {
+        name: 'What We Work'
+      },
+      {
+        name: 'Careers'
+      },
+      {
+        name: 'News'
+      },
+    ]
+    };
+  } 
+}
 </script>
 
-<template></template>
+<template>
+  <PageHeader :linksInfo="links"></PageHeader>
+</template>
 
 <style lang="scss">
 @use './style/generic.scss';
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+    props: {
+        linksInfo: Array
+    },
+    data() {
+        return {
+            activeItem: 0
+        }
+    },
+    methods: {
+        activeLink(index){
+            this.activeItem = index;
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/images/logo_seo_w_1x.png" alt="AvadaSEO logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="linkInfo, index in linksInfo" class="nav-item d-flex align-items-center" @click="activeLink(index)">
+                    <a class="nav-link" :class="{'active': index == activeItem}">{{ linkInfo.name }}</a>
+                    <button v-if="linkInfo.name === 'Careers'" class="btn btn-outline-primary">apply</button>
+                </li>
+                <button class="btn btn-success rounded-pill">get in touch now</button>
+            </ul>
+        </div>
+    </div>
+</nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+header {
+    background-color: red;
+
+    .navbar {
+        .nav-link {
+            color: $brand-light;
+            cursor: pointer;
+
+            &.active {
+                color: $brand-primary;
+            }
+
+            &:hover {
+                color: $brand-primary;
+            }
+        }
+    }
+}
+</style>
